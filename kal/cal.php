d75a02b188325279e16c21dc32c97c3a3fd5a73f
Roodewald <55383286+Roodewald@users.noreply.github.com>
2020-03-03 00:45:51 +0500

    Add files via upload

--- kal/cal.php
+++ kal/cal.php
@@ -11,43 +11,57 @@ Author URI: https://spktt.ru
 function html_to_code(){
     ?>
     <form method ="post" onsubmit="submitMe(); return false">
-        <p>Ваше имя <br/>
-            <input type="text" id="name" name="cf-name" size="40" />
+        <p>Преподователь <br/>
+            <input type="radio" id="prepod1" name="prepod" value="500">
+                Высшей категории 500р/ч <br/>
+            <input type="radio" id="prepod2" name="prepod" value="300">
+                Первой категории 300р/ч 
+
         </p>
-        <p>Почта для связи <br/>
-            <input type="email" id="email" name="cf-surname" size="40" />
+        <p>Курс<br/>
+            <input type="radio" id="curse1" name="curse" value="0.5">
+                Для школьников <br/>
+            <input type="radio" id="curse2" name="curse" value="0.7">
+                Для студентов <br/>
+            <input type="radio" id="curse3" name="curse" value="2">
+                Для рабочих <br/>
+            <input type="radio" id="curse4" name="curse" value="1">
+                Для Пенсионеров <br/>
+            
         </p>
-        <p>Предпочетаемное имя (required) <br/>
-            <input type="text" id="subject" name="cf-subject" size="40" />
+        <p>Город <br/>
+            <input type="radio" id="city1" name="city" value="2">
+                Ханты-Мансийск <br/>
+            <input type="radio" id="city2" name="city" value="3">
+                Сургут <br>
+            <input type="radio" id="city3" name="city" value="1.5">
+                Нижневартовск <br/>
+            <input type="radio" id="city4" name="city" value="1.2">
+                Когалым <br>
+            <input type="radio" id="city5" name="city" value="1.3">
+                Советский <br>
         </p>
-        <p>Пожелание (required) <br/>
-            <textarea rows="10" id="text" cols="35" name="cf-message"></textarea>
+        <p>Интенсивность<br/>
+            <input type="radio" id="intence1" name="intence" value="72">
+                72часа <br/>
+            <input type="radio" id="intence2" name="intence" value="144">
+                144 часа<br/>
+            <input type="radio" id="intence3" name="intence" value="288">
+                288 часа<br/>
         </p>
-        <p><input type="submit" name="cf-submitted" value="Send"></p>
+        <p><input type="submit" name="submitted" value="Send"></p>
     </form>
     <script>
         function submitMe() {
-            jQuery(function($) {
-                $.ajax( {
-                    url : "wp-content/plugins/SAH/messages_act.php?act=0",
-                    type : "POST",
-                    data: {
-                        "cf-name": document.getElementById('name').value,
-                        "cf-mail": document.getElementById('email').value,
-                        "cf-subject": document.getElementById('subject').value,
-                        "cf-message": document.getElementById('text').value
-                    },
-                    success : function(data) {
-                        console.log(data)
-                        alert (data);
-                    },
-                    error: function (request, status, error) {
-                        alert(error);
-                        console.log(request);
-                        console.log(status);
-                    }
-                });
-            })
+            var prepod  = document.getElementsByTagName('prepod').value;
+            var curse   = document.getElementsByTagName('curse').value;
+            var city    = document.getElementsByTagName('city').value;
+            var intence = document.getElementsByTagName('intence').value;
+            
+            var finalresult = prepod * curse * city * intence;
+
+            alert(curse);
+
         }
     </script>
     <?php
